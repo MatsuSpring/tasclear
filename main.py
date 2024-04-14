7b9cc25830521845b4a2b90d9bd5071ffd2b2889
harum <haru.mattsu0415@gmail.com>
2024-04-14 23:00:49 +0900

    taskFieldクラス＋そのコンストラクタの作成

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import flet as ft
 
+AppName = 'MyToDo(仮)'
+
+class taskField(ft.UserControl):
+    def __init__(self, properties):
+        taskId = properties[0]
+        taskName = properties[1]
+        taskDeadline = properties[2]
+        taskCompleted = properties[3]
+        
+
 
 def main(page: ft.Page):
+    page.title = AppName
     page.theme_mode = 'light'
     theme = ft.Theme(
         color_scheme_seed=ft.colors.GREEN_500
@@ -11,7 +22,7 @@ def main(page: ft.Page):
     page.appbar = ft.AppBar(
         leading=ft.Icon(ft.icons.FACT_CHECK_ROUNDED),
         leading_width=40,
-        title=ft.Text('MyToDo'),
+        title=ft.Text(AppName),
         center_title=True,
         bgcolor=theme.color_scheme_seed
     )
