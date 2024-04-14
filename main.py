278f0ff9f2d47a3107753e5079e1310f687e6a03
harum <haru.mattsu0415@gmail.com>
2024-04-14 22:43:41 +0900

    テーマの設定とAppBarの設置￥

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.theme_mode = 'light'
+    theme = ft.Theme(
+        color_scheme_seed=ft.colors.GREEN_500
+    )
+    page.theme = theme
+    page.dark_theme = theme
+    page.appbar = ft.AppBar(
+        leading=ft.Icon(ft.icons.FACT_CHECK_ROUNDED),
+        leading_width=40,
+        title=ft.Text('MyToDo'),
+        center_title=True,
+        bgcolor=theme.color_scheme_seed
+    )
 
 
+    page.add()
+    
 ft.app(main)
